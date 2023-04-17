f008196ae0fefb4c0a383a10523fb0ce4770228c
Srinivas-162003 <100286844+Srinivas-162003@users.noreply.github.com>
2023-04-17 11:55:57 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+//K.Srinivas
+//21070126050
+//AIML-A3
+
+package Bird;
+public class Main {
+    public static void main(String[] args){
+
+        Bird.MallarDuck mallarduck= new Bird.MallarDuck();
+        mallarduck.display();
+        mallarduck.performFly();
+        mallarduck.performQuack();
+    }
+}
